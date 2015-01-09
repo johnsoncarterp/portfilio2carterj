29dddd84aa68cb1a557cf0e6a8aad0cca7737ccd
carter johnson <carter32lax@yahoo.com>
2015-01-09 14:02:33 -0800

    almost done with milestone 4

--- public_html/script.js
+++ public_html/script.js
@@ -9,10 +9,39 @@ $("document").ready(function() {
     $(".practice").css("background-color", "cyan");
     $('p:last').css({"background-color": "green", "color": "white"});
     $('a').css({"background-color": "black", "color": "white"});
-    $('carter').bind('click',alertButtonClick);
+    $("#carter").bind('click',alertButtonClick);
+    $("h1").bind('click', mouseClick);
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    $("#cj").bind('click', replaceWText);
+    $("#apple").bind('click', removeAPara);
+    $("#cool").bind('click', addAPara)
 });
 
 
 function alertButtonClick() {
     alert("there was a button clicked");
+}
+
+function mouseOverMe() {
+    $('h1').html('carter');
+}
+
+function mouseOutMe() {
+    $('h1').html('hello');
+}
+
+function mouseClick() {
+    $('p').html('clicked');
+}
+
+function replaceWText() {
+    $('h3').html('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+}
+
+function removeAPara() {
+    $('#apple p:last').remove();
+}
+
+function addAPara() {
+    $('#cool').append('<p>another paragraph</p>');
 }
